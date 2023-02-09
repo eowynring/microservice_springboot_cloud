c5654590fc3bab34aaff15220cdc21d16e5f1e80
guofei <guogoffy@outlook.com>
2023-02-09 12:43:42 +0800

    add kafka with springboot

---
+++ spring-boot-kafka/src/main/java/com/microservice/controller/KafkaController.java
@@ -0,0 +1,17 @@
+package com.microservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+    private final static String TOPIC_NAME = "myKafka-topic";
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    @RequestMapping("/send")
+    public void send() {
+        kafkaTemplate.send(TOPIC_NAME, 0 , "key", "this is a msg");
+    }
+}
---
+++ spring-boot-kafka/src/main/java/com/microservice/listener/MyKafkaConsumer.java
@@ -0,0 +1,20 @@
+package com.microservice.listener;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyKafkaConsumer {
+
+    @KafkaListener(topics = "myKafka-topic",groupId = "testGroup")
+    public void listenGroup(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        String value = record.value();
+        System.out.println(value);
+        System.out.println(record);
+        //手动提交offset
+        ack.acknowledge();
+    }
+
+}
